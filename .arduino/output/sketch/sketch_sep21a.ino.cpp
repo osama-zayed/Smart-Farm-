70d4ae3823f37cd362e44dbd389e291f117131aa
osama-zayed <o.a.s775561590@gmail.com>
2024-09-21 03:20:22 +0300

    add new

--- .arduino/output/sketch/sketch_sep21a.ino.cpp
+++ .arduino/output/sketch/sketch_sep21a.ino.cpp
@@ -2,18 +2,23 @@
 #line 1 "H:\\sketch_sep18a\\New folder\\sketch_sep21a\\sketch_sep21a.ino"
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h> // إضافة المكتبة هنا
+#include <ArduinoJson.h>
 
 const char* ssid = "osama zayed"; // استبدل بـ SSID الخاص بك
 const char* password = "osama775561"; // استبدل بكلمة المرور الخاصة بك
 
-const String apiKey = "2YM7M7O0JBS4DY9E"; // مفتاح API الخاص بالقراءة
+const String apiKey = "2YM7M7O0JBS4DY9E"; // مفتاح API الخاص بالقناة
 const String channelID = "2662884"; // قناة ThingSpeak
-const int ledPin = 23; // رقم GPIO المتصل بالـ LED
+const int ledPin1 = 23; // رقم GPIO المتصل بالـ LED للحقل الأول
+const int ledPin2 = 22; // رقم GPIO المتصل بالـ LED للحقل الثاني
+
+int lastLedState1 = -1; // لتتبع حالة اللمبة للحقل الأول
+int lastLedState2 = -1; // لتتبع حالة اللمبة للحقل الثاني
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
   
   // الاتصال بشبكة WiFi
   WiFi.begin(ssid, password);
@@ -31,7 +36,7 @@ void loop() {
     HTTPClient http;
     
     // إعداد عنوان URL لقراءة البيانات من ThingSpeak
-    String url = "http://api.thingspeak.com/channels/" + channelID + "/fields/1.json?api_key=" + apiKey + "&results=1";
+    String url = "http://api.thingspeak.com/channels/" + channelID + "/feeds.json?api_key=" + apiKey + "&results=1";
     
     http.begin(url); // إعداد عنوان URL
     int httpResponseCode = http.GET(); // إجراء الطلب GET
@@ -41,18 +46,48 @@ void loop() {
       Serial.println(httpResponseCode); // طباعة رمز الاستجابة
       Serial.println(response); // طباعة الاستجابة
       
-      // تحليل JSON لاستخراج قيمة الحقل
+      // تحليل JSON لاستخراج قيمة الحقول
       DynamicJsonDocument doc(1024);
       deserializeJson(doc, response);
-      int ledState = doc["feeds"][0]["field1"]; // الحصول على قيمة الحقل
+      
+      // قراءة حالة اللمبات من الحقول
+      String ledState1 = doc["feeds"][0]["field1"]; // الحصول على قيمة الحقل الأول
+      String ledState2 = doc["feeds"][0]["field2"]; // الحصول على قيمة الحقل الثاني
 
-      // تشغيل أو إيقاف تشغيل LED بناءً على القيمة
-      if (ledState == 1) {
-        digitalWrite(ledPin, HIGH); // تشغيل LED
-        Serial.println("LED is ON");
-      } else {
-        digitalWrite(ledPin, LOW); // إيقاف تشغيل LED
-        Serial.println("LED is OFF");
+      // تحويل القيم إلى int
+      int state1 = (ledState1 != "null") ? ledState1.toInt() : lastLedState1;
+      int state2 = (ledState2 != "null") ? ledState2.toInt() : lastLedState2;
+
+      // طباعة القيم
+      Serial.print("LED 1 State: ");
+      Serial.println(state1);
+      Serial.print("LED 2 State: ");
+      Serial.println(state2);
+
+      // التحكم في LED للحقل الأول
+      if (state1 != lastLedState1) {
+        lastLedState1 = state1;
+
+        if (state1 == 1) {
+          digitalWrite(ledPin1, HIGH);
+          Serial.println("LED 1 is ON");
+        } else {
+          digitalWrite(ledPin1, LOW);
+          Serial.println("LED 1 is OFF");
+        }
+      }
+
+      // التحكم في LED للحقل الثاني
+      if (state2 != lastLedState2) {
+        lastLedState2 = state2;
+
+        if (state2 == 1) {
+          digitalWrite(ledPin2, HIGH);
+          Serial.println("LED 2 is ON");
+        } else {
+          digitalWrite(ledPin2, LOW);
+          Serial.println("LED 2 is OFF");
+        }
       }
 
     } else {
@@ -65,6 +100,7 @@ void loop() {
     Serial.println("WiFi Disconnected");
   }
 
-  // الانتظار لمدة 10 ثوانٍ قبل التكرار
-  delay(10000); // الانتظار بين الاستعلامات (10 ثوانٍ)
+  // الانتظار لمدة 5 ثوانٍ قبل التكرار
+  delay(5000); 
 }
+
